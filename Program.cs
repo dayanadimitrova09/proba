4a1672bccb1e294dbb25704b4300d8e231105956
dayanadimitrova09 <dd09681635@edu.mon.bg>
2024-09-30 13:06:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Name:");
+string name=Console.ReadLine();
+Console.Write("Date of birth:");
+string date=Console.ReadLine();
+Console.Write("Place of birth:");
+string place=Console.ReadLine();
+Console.Write("School:");
+string school=Console.ReadLine();
+Console.Write("Class:");
+string klas=Console.ReadLine();
+Console.Write("Number:");
+string num=Console.ReadLine();
+Console.Write("Profession:");
+string prof=Console.ReadLine();
+Console.WriteLine(name);
+Console.WriteLine("Born on " + date);
+Console.WriteLine("From school "+school+", "+place+", "+klas+" class, number "+num);
+Console.WriteLine("Profession " + prof);
